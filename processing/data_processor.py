85aee8e09f9e5a7ffe5faafe17679a8aea6a4283
Beatrice Marti <mabesa@users.noreply.github.com>
2024-12-04 22:16:39 +0100

    Update data_processor.py

--- processing/data_processor.py
+++ processing/data_processor.py
@@ -442,12 +442,25 @@ class SnowDataPipeline:
                             .assign_coords(time=forecast_times))
             self.logger.debug(f"Forecast data shape: {forecast_data[var_name].shape}")
 
+            forecast_copy = forecast_data.copy(deep=True)
+            self.logger.debug(f"shape of forecast_copy: {forecast_copy[var_name].shape}")
+            # print names of variables
+            self.logger.debug(f"Variable names: {forecast_copy.data_vars}")
+
+            # Take the difference between time steps to get new snowfall
+            forecast_copy[var_name] = forecast_copy[var_name].diff(dim='time', n=1)
+            self.logger.debug(f"shape of forecast_copy: {forecast_copy[var_name].shape}")
+
+            # Only keep values >= 0
+            forecast_copy[var_name] = forecast_copy[var_name].where(forecast_copy[var_name] >= 0)
+
             # Calculate accumulations with same time coordinates
-            accumulated = (forecast_data.cumsum(dim='time')
+            accumulated = forecast_copy.copy(deep=True  )
+            accumulated[var_name] = (accumulated[var_name].cumsum(dim='time')
                           .assign_coords(time=forecast_times))
 
             # Subtract the first time step to get the actual values
-            accumulated = accumulated - accumulated.isel(time=0)
+            #accumulated[var_name] = accumulated[var_name] - accumulated[var_name].isel(time=0)
 
             # Rename variables for forecast and accumulated
             forecast_data = forecast_data.rename({var_name: f"{var_name}_time_series"})
